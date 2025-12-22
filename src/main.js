5e3d0302d4dd5cc14fa236b66e9e38437942d618
domanskey <domadam04@gmail.com>
2025-12-22 23:50:01 +0100

    add show password functionality

--- src/main.js
+++ src/main.js
@@ -1,2 +1,43 @@
 import './style.scss'
 
+const password = document.getElementById('password');
+const passwordConfirmation = document.getElementById('passwordConfirmation');
+
+const toggleButton = document.getElementById('toggle-password');
+const toggleButtonConfirmation = document.getElementById('toggle-password-confirmation');
+
+function showPassword(input, button) {
+  const isVisible = input.type === 'text';
+
+  if (isVisible) {
+    // Ukrywamy hasło
+    input.type = 'password'; 
+    button.classList.remove('visible');
+    button.setAttribute('aria-pressed', 'false');
+  } else {
+    // Pokazujemy hasło
+    input.type = 'text';
+    button.classList.add('visible');
+    button.setAttribute('aria-pressed', 'true');
+  }
+}
+
+toggleButton.addEventListener('click', () => {showPassword(password, toggleButton)});
+toggleButtonConfirmation.addEventListener('click', () => {showPassword(passwordConfirmation, toggleButtonConfirmation)});
+
+
+
+
+
+
+// const warningElement = document.getElementById('password-warning');
+
+// function showWarning(message) {
+//   warningElement.textContent = message;
+//   warningElement.classList.add('is-visible');
+// }
+
+// function hideWarning() {
+//   warningElement.classList.remove('is-visible');
+//   // Opcjonalnie: wyczyść tekst po animacji, aby zwolnić miejsce
+// }
